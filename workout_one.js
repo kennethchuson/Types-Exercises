5588408c85ced36255a91774d1af1fdad2a98419
kennethchuson <kennethchuson@yahoo.com>
2022-01-14 14:31:36 -0800

    ran workout one

--- workout_one.js
+++ workout_one.js
@@ -1,2 +1,20 @@
-var id = 5;
-console.log(id);
+var solve1 = function (arr) {
+    var store_number = [];
+    var store_string = [];
+    var sum = 0;
+    arr.map(function (x) {
+        if (typeof (x) === "number") {
+            sum += x;
+            store_number.push(sum);
+        }
+        if (typeof (x) === "string") {
+            if (x.indexOf("ing") !== -1) {
+                store_string.push(x);
+            }
+        }
+    });
+    return [store_number, store_string];
+};
+var arr1 = ["test", 1, 2, "testing2", undefined, 3];
+var output1 = solve1(arr1);
+console.log(output1);
